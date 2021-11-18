943a8eb7050a9646614e5a75188db3f01e835007
Raphael Maciel Vilela <rap_mv@yahoo.com.br>
2021-11-18 16:15:37 -0300

    H2 database

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -3,11 +3,22 @@ package com.devsuperior.dscatalog.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="tb_category")
 public class Category implements Serializable{
 	
 	//conversão do objeto em bytes (colocado por conta de ser uma boa medida)
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY )
 	private Long id;
 	private String name;
 	
